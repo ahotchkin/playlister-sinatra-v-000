994d05e4ae0129565a73a3620fc28587862c3065
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-01-12 15:46:14 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,11 +17,18 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
+<<<<<<< HEAD
     @song = Song.create(name: params[:song_name])
     if !params[:artist_name].empty?
       @song.artist << Artist.create(name: params[:artist_name])
     end
     flash[:notice] = "Successfully created song."
+=======
+    @song = Song.create(params[:song])
+    if !params[:artist][:name].empty?
+      @song.artist << Artist.create(params[:artist])
+    end
+>>>>>>> cb348e1480e9c1e238b1cc67c0abe15f8adfa15b
     redirect "/songs/#{@song.slug}"
   end
 
